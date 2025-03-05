d6d34b7f0fa1e627a047eefbfc7dfa568debafd7
doona1 <shambadi01@gmail.com>
2025-03-06 03:44:57 +0530

    done till creating components and importing to app.js

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
